7a4a5e0886c44a3a3ef35d0f9facf5dc53e09ae7
christianjamet-wq <christian.jamet@savouest.leclerc>
2026-05-02 16:30:28 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Service worker qui se desinstalle lui-meme
-self.addEventListener('install', function() { self.skipWaiting(); });
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
-    }).then(function() { return self.clients.claim(); })
-  );
-});
